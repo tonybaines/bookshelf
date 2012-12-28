a47d106de1a51894caa0a87005609358df74f0a5
Tony Baines <tony.baines.1@gmail.com>
2012-12-28 23:10:29 +0000

    A more concise way of getting a JSON response body

--- app/controllers/BookController.scala
+++ app/controllers/BookController.scala
@@ -4,27 +4,17 @@ import play.api.mvc._
 import play.api.libs.json.{JsArray, JsValue, Writes, Json}
 
 // be careful which Json object gets imported
+
 import models.Book
 
-object BookController extends Controller{
+object BookController extends Controller {
   def index = Action {
-    val books = BooksWrites.writes(Book.findAll)
-    Ok(books)
-  }
-
-
-  // Converters from model to JSON
-  implicit object BookWrites extends Writes[Book] {
-    def writes(b: Book) = Json.toJson(
+    val books = Json.toJson(Book.findAll.map { b =>
       Map(
         "id" -> Json.toJson(b.id),
         "title" -> Json.toJson(b.title)
       )
-    )
-  }
-  implicit object BooksWrites extends Writes[Iterable[Book]] {
-    def writes(books: Iterable[Book]) = JsArray(
-      books.map(b => BookWrites.writes(b)).toSeq
-    )
+    } toSeq)
+    Ok(books)
   }
 }
